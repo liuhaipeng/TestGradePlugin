e7a28931d773581bc4141f360499f49b40f7d1a3
liuhaipeng <liuhaipeng@foryou56.com>
2019-05-15 15:32:59 +0800

    Initial commit

---
+++ plugin/src/main/groovy/com.lhp.gradle/PluginImpl.groovy
@@ -0,0 +1,16 @@
+package com.lhp.gradle
+
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+
+public class PluginImpl implements Plugin<Project> {
+    void apply(Project project) {
+        project.task('testTask') {
+            println "+++++++++++++++++++++++++"
+            println "Hello gradle plugin"
+            println "+++++++++++++++++++++++++"
+        }
+    }
+
+
+}
